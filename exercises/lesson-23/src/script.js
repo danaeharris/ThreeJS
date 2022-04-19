071261491df6bab991207c5a87986053b7604abf
Danae Harris <hello@danaeharris.com>
2022-04-19 17:08:17 -0400

    Lesson 23 mid-lesson.

---
+++ exercises/lesson-23/src/script.js
@@ -0,0 +1,207 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader();
+console.log({ gltfLoader });
+
+// gltf
+// gltfLoader.load(
+//   "/models/Duck/glTF/Duck.gltf",
+//   (gltf) => {
+//     console.log("success");
+//     console.log(gltf);
+//     scene.add(gltf.scene.children[0]);
+//   },
+//   (progress) => {
+//     console.log({ progress });
+//   },
+//   (error) => {
+//     console.log({ error });
+//   }
+// );
+gltfLoader.load(
+  "/models/FlightHelmet/glTF/FlightHelmet.gltf",
+  (gltf) => {
+    console.log("success");
+    // as we add a child to the scene, it will remove the child from the gltf scene.
+    // This is why we can't just forEach over the scene.
+    // we won't get all the children.
+
+    // option 1 While loop
+    // while loops can freeze chrome if you mess them up.
+    // while (gltf.scene.children.length > 0) {
+    //   scene.add(gltf.scene.children[0]);
+    // }
+
+    // option 2 duplicate the scene and then for over it
+    // const children = [...gltf.scene.children];
+    // for (const child of children) {
+    //   scene.add(child);
+    // }
+
+    // option 3 add the whole scene (you might add things you don't need)
+    scene.add(gltf.scene);
+  },
+  (progress) => {
+    console.log({ progress });
+  },
+  (error) => {
+    console.log({ error });
+  }
+);
+
+// glb
+// gltfLoader.load(
+//   "/models/Duck/glTF-Binary/Duck.glb",
+//   (gltf) => {
+//     console.log("success");
+//     console.log(gltf);
+//     scene.add(gltf.scene.children[0]);
+//   },
+//   (progress) => {
+//     console.log({ progress });
+//   },
+//   (error) => {
+//     console.log({ error });
+//   }
+// );
+
+// gltf embedded
+// gltfLoader.load(
+//   "/models/Duck/glTF-Embedded/Duck.gltf",
+//   (gltf) => {
+//     console.log("success");
+//     console.log(gltf);
+//     scene.add(gltf.scene.children[0]);
+//   },
+//   (progress) => {
+//     console.log({ progress });
+//   },
+//   (error) => {
+//     console.log({ error });
+//   }
+// );
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.5,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(2, 2, 2);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 0.75, 0);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
